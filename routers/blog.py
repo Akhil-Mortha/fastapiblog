9d6aa2154f057a30641f510f4b45dfbf16e72cb2
Akhil Mortha <akhil@example.com>
2026-02-10 19:49:56 +0530

    Updates

--- routers/blog.py
+++ routers/blog.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 
-@router.get('/',response_model=List[schemas.ShowUser])# if u want other creators too
+@router.get('/',response_model=List[schemas.ShowBlog])# if u want other creators too
 def all(db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
